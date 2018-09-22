d5199c97cc849dbe983966c4e338789580dc86a5
anhtaka <email@example.com>
2018-09-22 22:35:09 +0900

    show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'top' => 'home#top'
   get 'about' => 'home#about'
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
