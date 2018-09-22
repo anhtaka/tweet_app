42c1963933a146b82afa7869f643895acc705605
anhtaka <email@example.com>
2018-09-23 00:20:38 +0900

    new

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'home#about'
 
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
