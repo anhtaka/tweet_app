b94620c79afbfb6978cc121543503f00082e9c04
anhtaka <email@example.com>
2018-09-24 19:12:15 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'posts/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
   get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
