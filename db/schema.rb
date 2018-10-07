9165cf24f3dfccb5496c82c98274c5bbd0f2c1f2
anhtaka <email@example.com>
2018-10-07 13:51:35 +0900

    user2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_155540) do
+ActiveRecord::Schema.define(version: 2018_10_04_162542) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
