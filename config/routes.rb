b4b08d8b5b690fbc2a41f0ce1a2a0b70af86ff55
anhtaka <email@example.com>
2018-10-12 00:08:32 +0900

    bag

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get  'users/index'
-  post 'users/create' => 'users#create'
-  get  'users/:id' => 'users#show'
-  get  'signup' => 'users#new'
-  get  'users/:id/edit' => 'users#edit'
-  post 'users/:id/update' => 'users#update'
-  get  'login' => 'users#login_form'
-  post 'login' => 'users#login'
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
   post "logout" => "users#logout"
+  get "login" => "users#login_form"
 
   get 'posts/index'
   get '/' => 'home#top'
